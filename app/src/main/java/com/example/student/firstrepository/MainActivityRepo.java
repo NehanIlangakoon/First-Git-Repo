71670611c5dce557cbb8e1e3e77bf1053a0ebf10
NehanIlangakoon <nehanilangakoon1@gmail.com>
2019-07-19 13:33:59 +0530

    This is my first commit..

---
+++ app/src/main/java/com/example/student/firstrepository/MainActivityRepo.java
@@ -0,0 +1,13 @@
+package com.example.student.firstrepository;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityRepo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_repo);
+    }
+}
